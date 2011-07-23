8c3ca755cea55e924b8c751c338537a271819bca
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2011-07-23 15:37:01 -0500

    New PeopleList widget.

---
+++ Views/Widgets/PeopleList.cs
@@ -0,0 +1,67 @@
+using System;
+using HumanRightsTracker.Models;
+namespace Views
+{
+    [Gtk.TreeNode (ListOnly=true)]
+    public class PersonNode : Gtk.TreeNode
+    {
+        public PersonNode (Person person)
+        {
+            Person = person;
+            Photo = "";
+            //TODO Fullname property for Person model.
+            Name = person.Firstname + " " +  person.Lastname;
+        }
+
+        public Person Person;
+
+        [Gtk.TreeNodeValue (Column=0)]
+        public string Photo;
+
+        [Gtk.TreeNodeValue (Column=1)]
+        public string Name;
+    }
+
+
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class PeopleList : Gtk.Bin
+    {
+        Person[] people;
+
+        public event EventHandler SelectionChanged;
+
+        public PeopleList ()
+        {
+        this.Build ();
+            tree.NodeStore = Store;
+            tree.AppendColumn ("Photo", new Gtk.CellRendererText (), "text", 0);
+            tree.AppendColumn ("Name", new Gtk.CellRendererText (), "text", 1);
+
+            tree.NodeSelection.Changed += new System.EventHandler (OnSelectionChanged);
+        }
+
+        protected void OnSelectionChanged (object o, System.EventArgs args)
+        {
+            Gtk.NodeSelection selection = (Gtk.NodeSelection) o;
+            PersonNode node = (PersonNode) selection.SelectedNode;
+            if (SelectionChanged != null)
+                SelectionChanged(node.Person, args);
+        }
+
+        Gtk.NodeStore store;
+        Gtk.NodeStore Store {
+            get {
+                if (store == null) {
+                    people = Person.FindAll();
+
+                    store = new Gtk.NodeStore (typeof (PersonNode));
+
+                    foreach (Person p in people)
+                        store.AddNode(new PersonNode(p));
+                }
+                return store;
+            }
+        }
+    }
+}
+
