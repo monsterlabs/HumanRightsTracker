fd92a9b12f7ebfebcb2a962f3250e86eeee4a25c
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2011-07-26 10:27:09 -0500

    DetailedDateSelector and DateTypeandDateSelector

---
+++ Views/Widgets/DateTypeAndDateSelector.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class DateTypeAndDateSelector : Gtk.Bin
+    {
+        public DateTypeAndDateSelector ()
+        {
+            this.Build ();
+        }
+    }
+}
+
---
+++ Views/Widgets/DetailedDateSelector.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class DateFieldsSelector : Gtk.Bin
+    {
+        public DateFieldsSelector ()
+        {
+            this.Build ();
+        }
+    }
+}
+
