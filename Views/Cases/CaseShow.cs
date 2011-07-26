244d67e9995d7e4314b03e7c4ff53a1bf8fa377f
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2011-07-26 00:17:57 -0500

    Adding views for cases

---
+++ Views/Cases/CaseShow.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class CaseShow : Gtk.Bin
+    {
+        public CaseShow ()
+        {
+            this.Build ();
+        }
+    }
+}
+
---
+++ Views/Cases/CasesTab.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class CasesTab : Gtk.Bin
+    {
+        public CasesTab ()
+        {
+            this.Build ();
+        }
+    }
+}
+
