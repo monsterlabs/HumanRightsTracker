5b18eb23ccb904e7304356a9135ac521623d2a21
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2011-08-01 18:23:42 -0500

    CaseEdit Widget to replace CaseShow widget

---
+++ Views/Cases/CaseEdit.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class CaseEdit : Gtk.Bin
+    {
+        public CaseEdit ()
+        {
+            this.Build ();
+        }
+    }
+}
+
