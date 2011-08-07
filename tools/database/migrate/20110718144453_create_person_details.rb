0e1c678adee5f6ca3109db2ccc4bc67d1b56a01a
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2011-08-06 22:10:30 -0500

    Renaming number_of_children with number_of_son in person_details table

--- tools/database/migrate/20110718144453_create_person_details.rb
+++ tools/database/migrate/20110718144453_create_person_details.rb
@@ -2,7 +2,7 @@ class CreatePersonDetails < ActiveRecord::Migration
   def self.up
     create_table :person_details do |t|
       t.references :person
-      t.integer :number_of_children
+      t.integer :number_of_sons
       t.references :ethnic_group, :religion, :scholarity_level
       t.string :indigenous_group, :most_recent_job
     end
