557db3fc0e535b3043b58156a2cca75e1948ba50
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2011-10-21 02:10:37 -0500

    New victim show widget.

---
+++ Views/Victim/VictimShow.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class VictimShow : Gtk.Bin
+    {
+        public VictimShow ()
+        {
+            this.Build ();
+        }
+    }
+}
+
