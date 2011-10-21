8db3dbd6845c9c5c145240a0ac4cc6cb9b930e62
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2011-10-21 02:36:56 -0500

    ·Perpetrator widgets in initial state.

---
+++ Views/Perpetrator/PerpetratorRow.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class PerpetratorRow : Gtk.Bin
+    {
+        public PerpetratorRow ()
+        {
+            this.Build ();
+        }
+    }
+}
+
---
+++ Views/Perpetrator/PerpetratorSelector.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class PerpetratorSelector : Gtk.Bin
+    {
+        public PerpetratorSelector ()
+        {
+            this.Build ();
+        }
+    }
+}
+
