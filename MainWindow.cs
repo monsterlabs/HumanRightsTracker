04746662d7723c63e2f4ee1a1cb261fcb28aef86
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2011-11-01 14:49:21 -0600

    Tabs are now built on demand.

--- MainWindow.cs
+++ MainWindow.cs
@@ -12,6 +12,7 @@ namespace HumanRightsTracker
         {
             this.Build ();
             individual_peopletab.InitialSetup ();
+            notebook3.CurrentPage = 0;
             initialized = true;
             this.Default = individual_peopletab.DefaultButton ();
         }
@@ -20,17 +21,6 @@ namespace HumanRightsTracker
         {
             Application.Quit ();
             a.RetVal = true;
-        }
-        
-        protected void OnChangeTab (object o, Gtk.SwitchPageArgs args)
-        {
-            Gtk.Notebook n = o as Notebook;
-            TabWithDefaultButton tab = n.CurrentPageWidget as TabWithDefaultButton;
-            if (initialized && tab != null) {
-                tab.InitialSetup();
-                this.Default = tab.DefaultButton ();
-            }
-            return;
         }
 
         protected void onRemove (object sender, System.EventArgs e)
@@ -41,6 +31,18 @@ namespace HumanRightsTracker
         protected void onAdd (object sender, System.EventArgs e)
         {
             throw new System.NotImplementedException ();
+        }
+
+        protected void OnChangeTab (object o, Gtk.SwitchPageArgs args)
+        {
+            Gtk.Notebook n = o as Notebook;
+            TabWithDefaultButton tab = n.CurrentPageWidget as TabWithDefaultButton;
+            if (initialized && tab != null) {
+                tab.InitialSetup();
+                this.Default = tab.DefaultButton ();
+                n.CurrentPage = (int) args.PageNum;
+            }
+            return;
         }
     }
 }
