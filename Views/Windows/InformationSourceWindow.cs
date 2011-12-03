333324d4dce64ad51451fe8f4abba7733ed95092
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2011-12-03 06:03:31 -0600

    Adding InformationSource getter and setter in InformationSourceWindow.cs

--- Views/Windows/InformationSourceWindow.cs
+++ Views/Windows/InformationSourceWindow.cs
@@ -7,19 +7,54 @@ namespace Views
     {
         public event EventHandler OnInterventionSaved = null;
 
+        InformationSource information_source;
+        bool isEditable;
+
         public InformationSourceWindow () :
                 base(Gtk.WindowType.Toplevel)
         {
             this.Build ();
         }
 
-        public InformationSourceWindow (Case c, EventHandler onSave, Gtk.Window parent) : base(Gtk.WindowType.Toplevel)
+        public InformationSourceWindow (InformationSource i, EventHandler onSave, Gtk.Window parent) : base(Gtk.WindowType.Toplevel)
         {
             this.Build ();
             this.Modal = true;
             this.OnInterventionSaved = onSave;
             this.TransientFor = parent;
+            InformationSource = i;
         }
+
+         public InformationSource InformationSource {
+            get { return this.information_source; }
+            set {
+                information_source = value;
+                if (information_source != null) {
+                    affiliation_type.Active = information_source.AffiliationType;
+                    language.Active = information_source.Language;
+                    indigenous_language.Active = information_source.IndigenousLanguage;
+                    reliability_level.Active = information_source.ReliabilityLevel;
+                    observations.Buffer.Text = information_source.Observations;
+                    comments.Buffer.Text = information_source.Comments;
+                }
+            }
+        }
+
+         public bool IsEditable {
+            get {
+                return this.isEditable;
+            }
+            set {
+                isEditable = value;
+                affiliation_type.IsEditable = value;
+                language.IsEditable = value;
+                indigenous_language.IsEditable = value;
+                reliability_level.IsEditable = value;
+                observations.Editable = value;
+                comments.Editable = value;
+            }
+        }
+
     }
 }
 
