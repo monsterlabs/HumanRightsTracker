e30f8090126f417e2ed9833795bab99f87df757a
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2012-02-12 00:10:30 -0600

    CasesReportForm widget added. Restored.

---
+++ Views/Reports/CasesReportForm.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using HumanRightsTracker.Models;
+using Reports;
+using NHibernate.Criterion;
+
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class CasesReportForm : Gtk.Bin
+    {
+        public CasesReportForm ()
+        {
+            this.Build ();
+        }
+
+        protected void OnSave (object sender, System.EventArgs e)
+        {
+            ICriterion[] criteria = new ICriterion[] {
+                Restrictions.InsensitiveLike("Name", caseName.Text, MatchMode.Anywhere) };
+            Case[] cases = Case.FindAll (criteria);
+            CasesReportGenerator report = new CasesReportGenerator (cases);
+            report.SaveTo (Path.Combine(folderChooser.CurrentFolder, fileName.Text));
+        }
+    }
+}
---
+++ Views/Reports/ReportsTab.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Views
+{
+    [System.ComponentModel.ToolboxItem(true)]
+    public partial class ReportsTab : Gtk.Bin
+    {
+        public ReportsTab ()
+        {
+            this.Build ();
+        }
+    }
+}
+
