ec42454e4f32f4add737aa30797a0d748c08f063
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2012-02-25 01:04:12 -0600

    Completing case information in report.

--- Reports/CaseReportGenerator.cs
+++ Reports/CaseReportGenerator.cs
@@ -14,36 +14,43 @@ namespace Reports
     {
         public CaseReportGenerator (Case acase)
         {
-            addBold ("Reporte narrativo de caso");
-            addField("Fecha de expedición", DateTime.Now.ToShortDateString ());
+            //addBold ("Reporte narrativo de caso");
+            //addField("Fecha de expedición", DateTime.Now.ToShortDateString ());
 
-            addTitle ("DATOS GENERALES");
+            addTitle ("Información general");
 
             addField ("Nombre del caso", acase.Name);
+            addField ("Personas afectadas", acase.AffectedPeople.ToString ());
+
             addField ("Fecha de inicio", acase.start_date.Value.ToShortDateString ());
+            addField ("Fecha de término", acase.end_date.Value.ToShortDateString ());
             foreach (Place place in acase.Places)
             {
 
                 addField ("Localidades", String.Format ("{0}, {1}, {2}",
                                                       place.City.Name, place.State.Name, place.Country.Name));
             }
-            addField ("No. personas afectadas", acase.AffectedPeople.ToString ());
-
-            addTitle ("SEGUIMIENTO DEL CASO");
 
             addField ("Resumen", acase.Summary);
-            addField ("Observaciones", acase.Observations);
+
+            addTitle ("Observaciones");
+            addField ("", acase.Observations);
 
             addNewline();
 
+            addTitle ("Seguimiento del caso");
+
             foreach (TrackingInformation info in acase.TrackingInformation)
             {
-                addBold (info.Title);
+                addField ("Clave", info.RecordId.ToString ());
+                addField ("Título", info.Title);
+                addField ("Fecha de recepción", info.DateOfReceipt.Value.ToShortDateString ());
+                addField ("Estado", info.CaseStatus.Name);
                 addField ("Comentarios", info.Comments);
                 addNewline();
             }
 
-            addTitle ("DETALLES DE ACTOS");
+            addTitle ("Núcleo de casos");
 
             foreach (Act act in acase.Acts) {
                 addBold ("\t" + act.HumanRightsViolation.Name);
@@ -80,6 +87,63 @@ namespace Reports
                     }
                 }
 
+                addNewline ();
+            }
+
+            addTitle ("Intervenciones");
+
+            foreach (Intervention intervention in acase.Interventions) {
+                addField("Interventor", intervention.Interventor.Fullname);
+                addField("Institición", intervention.InterventorInstitution.Name);
+                if (intervention.InterventorAffiliationType != null)
+                    addField("Afiliación", intervention.InterventorAffiliationType.Name);
+
+                addField("Persona que soporta la intervención", intervention.Supporter.Fullname);
+                addField("Institición", intervention.SupporterInstitution.Name);
+                if (intervention.SupporterAffiliationType != null)
+                    addField("Afiliación", intervention.SupporterAffiliationType.Name);
+
+                addField("Impacto", intervention.Impact);
+                addField("Respuesta", intervention.Response);
+            }
+
+            addTitle ("Información adicional");
+
+            addTitle ("Fuentes documentales");
+
+            foreach (DocumentarySource docSource in acase.DocumentarySources) {
+                addField ("Nombre", docSource.Name);
+                //addField ("Tipo", docSource.ty);
+                addField ("Fecha de publicación", docSource.Date.Value.ToShortDateString ());
+                addField ("Fecha de acceso", docSource.AccessDate.Value.ToShortDateString ());
+                if (docSource.AdditionalInfo != null)
+                    addField ("Información adicional", docSource.AdditionalInfo);
+                if (docSource.ReliabilityLevel != null)
+                    addField ("Nivel de confiabilidod", docSource.ReliabilityLevel.Name);
+                addField ("Observaciones", docSource.Observations);
+                addNewline();
+            }
+
+            addTitle ("Fuentes de información personal");
+
+            foreach (InformationSource infoSource in acase.InformationSources) {
+                addField ("Nombre", infoSource.SourcePerson.Fullname);
+                addField ("Fecha", infoSource.Date.Value.ToShortDateString ());
+
+                addField ("Tipo de filiación", infoSource.SourceAffiliationType.Name);
+
+                addField ("Observaciones", infoSource.Observations);
+                addNewline ();
+            }
+
+            addTitle ("Relación entre casos");
+
+            foreach (CaseRelationship relation in acase.CaseRelationships) {
+                addField("Caso", relation.Case.Name);
+                addField ("Tipo de relación", relation.RelationshipType.Name);
+                addField("Caso relacionado", relation.RelatedCase.Name);
+                addField("Fecha de inicio", relation.RelatedCase.start_date.Value.ToShortDateString ());
+                addField("Fecha de término", relation.RelatedCase.end_date.Value.ToShortDateString ());
                 addNewline ();
             }
         }
