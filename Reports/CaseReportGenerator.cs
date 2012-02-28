74508fc302674314ae04fd14693127cd910d7547
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2012-02-28 00:54:17 -0600

    Counter verification per each section and minor changes were added in case report

--- Reports/CaseReportGenerator.cs
+++ Reports/CaseReportGenerator.cs
@@ -21,153 +21,194 @@ namespace Reports
 
             addField ("Nombre del caso", acase.Name);
             addField ("Personas afectadas", acase.AffectedPeople.ToString ());
-
             if (acase.start_date != null)
                 addField ("Fecha de inicio", acase.start_date.Value.ToShortDateString ());
             if (acase.end_date != null)
                 addField ("Fecha de término", acase.end_date.Value.ToShortDateString ());
-            foreach (Place place in acase.Places)
-            {
 
-                addField ("Localidades", String.Format ("{0}, {1}, {2}",
-                                                      place.City.Name, place.State.Name, place.Country.Name));
+            if (acase.Places.Count > 0 ) {
+                addTitle ("Lugares");
+                foreach (Place place in acase.Places)
+                {
+                    addField ("", String.Format ("{0}, {1}, {2}",
+                                  place.City.Name, place.State.Name, place.Country.Name));
+                }
             }
 
-            addField ("Resumen", acase.Summary);
+            addTitle ("Resumen");
+            addField ("", acase.Summary);
 
             addTitle ("Observaciones");
             addField ("", acase.Observations);
 
             addNewline();
 
-            addTitle ("Seguimiento del caso");
-
-            foreach (TrackingInformation info in acase.TrackingInformation)
+            if (acase.TrackingInformation.Count > 0)
             {
-                addField ("Clave", info.RecordId.ToString ());
-                addField ("Título", info.Title);
-                if (info.DateOfReceipt != null)
-                    addField ("Fecha de recepción", info.DateOfReceipt.Value.ToShortDateString ());
-                if (info.CaseStatus != null)
-                    addField ("Estado", info.CaseStatus.Name);
-                addField ("Comentarios", info.Comments);
-                addNewline();
+                addTitle ("Seguimiento del caso");
+
+                foreach (TrackingInformation info in acase.TrackingInformation)
+                {
+                    addField ("Clave", info.RecordId.ToString ());
+                    addField ("Título", info.Title);
+                    if (info.DateOfReceipt != null)
+                        addField ("Fecha de recepción", info.DateOfReceipt.Value.ToShortDateString ());
+                    if (info.CaseStatus != null)
+                        addField ("Estado", info.CaseStatus.Name);
+                    addField ("Comentarios", info.Comments);
+                    addNewline();
+                }
             }
 
-            addTitle ("Núcleo de casos");
+            addTitle ("Núcleo del caso");
+            if (acase.Acts.Count > 0) {
+                addTitle ("Derechos afectados y actos");
+                foreach (Act act in acase.Acts) {
+                    if (act.HumanRightsViolationCategory != null)
+                        addField ("\tDerecho afectado", act.HumanRightsViolationCategory.Name);
+                    if (act.HumanRightsViolation != null)
+                        addField ("\tActo", act.HumanRightsViolation.Name);
+                    if (act.ActStatus != null)
+                        addField ("Estado",act.ActStatus.Name);
+                    addField ("\tNo. afectados", act.AffectedPeopleNumber.ToString ());
+                    addField ("\tResumen", act.Summary);
+                    if (act.start_date != null)
+                        addField ("\tFecha de inicio", act.start_date.Value.ToShortDateString ());
+                    if (act.end_date != null)
+                        addField ("\tFecha de término", act.end_date.Value.ToShortDateString ());
 
-            foreach (Act act in acase.Acts) {
-                if (act.HumanRightsViolation != null)
-                    addBold ("\t" + act.HumanRightsViolation.Name);
-                addNewline ();
-                if (act.ActStatus != null)
-                    addField ("Estado",act.ActStatus.Name);
-                addField ("\tNo. afectados", act.AffectedPeopleNumber.ToString ());
-                addField ("\tResumen", act.Summary);
-                if (act.start_date != null)
-                    addField ("\tFecha de inicio", act.start_date.Value.ToShortDateString ());
-                if (act.end_date != null)
-                    addField ("\tFecha de término", act.end_date.Value.ToShortDateString ());
-                addField ("\tObservaciones de la victima", act.VictimObservations);
+                    if (act.Victims.Count > 0)
+                    {
+                        addNewline();
+                        addBold ("\t\tVíctimas");
 
-                addBold ("\tVictimas");
-
-                foreach (Victim victim in act.Victims) {
-                    addField ("\t\tVictima", victim.Person.Fullname);
-                    addField ("\t\tCaracterísticas", victim.Characteristics);
-                    if (victim.VictimStatus != null)
-                        addField ("\t\tEstado", victim.VictimStatus.Name);
-                    if (victim.Person != null)
-                        addField ("\t\tIntentos de inmigración", victim.Person.ImmigrationAttempts.ToString());
-                    addBold ("\t\tPerpetradares");
-                    foreach (Perpetrator perpetrator in victim.Perpetrators) {
-                        if (perpetrator.Person != null)
-                            addField ("\t\t\tPerpetrador", perpetrator.Person.Fullname);
-                        if (perpetrator.Institution != null) {
-                            addField ("\t\t\tInstitución", perpetrator.Institution.Name);
+                        int victim_counter = 1;
+                        foreach (Victim victim in act.Victims)
+                        {
+                            addField (String.Format ("{0} {1}", "\t\t\tVíctima", victim_counter), victim.Person.Fullname);
+                            addField ("\t\t\tCaracterísticas", victim.Characteristics);
+                            if (victim.VictimStatus != null)
+                                addField ("\t\t\tEstado", victim.VictimStatus.Name);
+                            /*
+                            if (victim.Person.ImmigrationAttempts.Count > 0 )
+                                addField ("\t\t\tIntentos de inmigración", victim.Person.ImmigrationAttempts.ToString());
+                             */
+                            if (victim.Perpetrators.Count > 0 )
+                            {
+                                addBold ("\t\t\tPerpetradores");
+                                int perpetrator_counter = 1;
+                                foreach (Perpetrator perpetrator in victim.Perpetrators)
+                                {
+                                    if (perpetrator.Person != null)
+                                        addField (String.Format ("{0} {1}", "\t\t\t\tPerpetrador", perpetrator_counter), perpetrator.Person.Fullname);
+                                    if (perpetrator.Institution != null)
+                                        addField ("\t\t\t\tInstitución", perpetrator.Institution.Name);
+                                    if (perpetrator.PerpetratorType != null)
+                                        addField ("\t\t\t\tTipo de perpetrador", perpetrator.PerpetratorType.Name);
+                                    if (perpetrator.PerpetratorStatus != null)
+                                        addField ("\t\t\t\tEstado del perpetrador", perpetrator.PerpetratorStatus.Name);
+                                    if (perpetrator.InvolvementDegree != null)
+                                        addField ("\t\t\t\tGrado de involucramiento", perpetrator.InvolvementDegree.Name);
+                                    addNewline();
+                                    perpetrator_counter++;
+                                }
+                            }
+                            victim_counter++;
                         }
-                        if (perpetrator.PerpetratorType != null)
-                            addField ("\t\t\tTipo de perpetrador", perpetrator.PerpetratorType.Name);
-                        if (perpetrator.PerpetratorStatus != null)
-                            addField ("\t\t\tEstado del perpetrador", perpetrator.PerpetratorStatus.Name);
-                        if (perpetrator.InvolvementDegree != null)
-                            addField ("\t\t\tGrado de involucramiento", perpetrator.InvolvementDegree.Name);
+                        addNewline ();
                     }
                 }
-
                 addNewline ();
             }
 
-            addTitle ("Intervenciones");
+            if (acase.Interventions.Count > 0 )
+            {
+                addTitle ("Intervenciones");
 
-            foreach (Intervention intervention in acase.Interventions) {
-                if (intervention.Interventor != null)
-                    addField("Interventor", intervention.Interventor.Fullname);
-                if (intervention.InterventorInstitution != null)
-                    addField("Institición", intervention.InterventorInstitution.Name);
-                if (intervention.InterventorAffiliationType != null)
-                    addField("Afiliación", intervention.InterventorAffiliationType.Name);
-                if (intervention.Supporter != null)
-                    addField("Persona que soporta la intervención", intervention.Supporter.Fullname);
-                if (intervention.SupporterInstitution != null)
-                    addField("Institición", intervention.SupporterInstitution.Name);
-                if (intervention.SupporterAffiliationType != null)
-                    addField("Afiliación", intervention.SupporterAffiliationType.Name);
+                foreach (Intervention intervention in acase.Interventions) {
+                    if (intervention.Interventor != null)
+                        addField("Interventor", intervention.Interventor.Fullname);
 
-                addField("Impacto", intervention.Impact);
-                addField("Respuesta", intervention.Response);
+                    if (intervention.InterventorInstitution != null)
+                        addField("Institución", intervention.InterventorInstitution.Name);
+
+                    if (intervention.InterventorAffiliationType != null)
+                        addField("Afiliación", intervention.InterventorAffiliationType.Name);
+
+                    if (intervention.Supporter != null)
+                        addField("Persona que soporta la intervención", intervention.Supporter.Fullname);
+
+                    if (intervention.SupporterInstitution != null)
+                        addField("Institución", intervention.SupporterInstitution.Name);
+
+                    if (intervention.SupporterAffiliationType != null)
+                        addField("Tipo de afiliación", intervention.SupporterAffiliationType.Name);
+
+                    addField("Impacto", intervention.Impact);
+                    addField("Respuesta", intervention.Response);
+                    addNewline();
+                }
+
             }
+
 
             addTitle ("Información adicional");
 
-            addTitle ("Fuentes documentales");
+            if (acase.DocumentarySources.Count > 0 )
+            {
+                addTitle ("Fuentes documentales");
 
-            foreach (DocumentarySource docSource in acase.DocumentarySources) {
-                addField ("Nombre", docSource.Name);
-                //addField ("Tipo", docSource.ty);
-                if (docSource.Date != null)
-                    addField ("Fecha de publicación", docSource.Date.Value.ToShortDateString ());
-                if (docSource.AccessDate != null)
-                    addField ("Fecha de acceso", docSource.AccessDate.Value.ToShortDateString ());
-                if (docSource.AdditionalInfo != null)
-                    addField ("Información adicional", docSource.AdditionalInfo);
-                if (docSource.ReliabilityLevel != null)
-                    addField ("Nivel de confiabilidod", docSource.ReliabilityLevel.Name);
-                addField ("Observaciones", docSource.Observations);
-                addNewline();
-            }
-
-            addTitle ("Fuentes de información personal");
-
-            foreach (InformationSource infoSource in acase.InformationSources) {
-                if (infoSource.SourcePerson != null)
-                    addField ("Nombre", infoSource.SourcePerson.Fullname);
-                if (infoSource.Date != null)
-                    addField ("Fecha", infoSource.Date.Value.ToShortDateString ());
-
-                if (infoSource.SourceAffiliationType != null)
-                    addField ("Tipo de filiación", infoSource.SourceAffiliationType.Name);
-
-                addField ("Observaciones", infoSource.Observations);
-                addNewline ();
-            }
-
-            addTitle ("Relación entre casos");
-
-            foreach (CaseRelationship relation in acase.CaseRelationships) {
-                if (relation.Case != null)
-                    addField("Caso", relation.Case.Name);
-                if (relation.RelationshipType != null)
-                    addField ("Tipo de relación", relation.RelationshipType.Name);
-                if (relation.RelatedCase != null) {
-                    addField("Caso relacionado", relation.RelatedCase.Name);
-                    if (relation.RelatedCase.start_date != null)
-                        addField("Fecha de inicio", relation.RelatedCase.start_date.Value.ToShortDateString ());
-                    if (relation.RelatedCase.end_date != null)
-                        addField("Fecha de término", relation.RelatedCase.end_date.Value.ToShortDateString ());
+                foreach (DocumentarySource docSource in acase.DocumentarySources) {
+                    addField ("Nombre", docSource.Name);
+                    if (docSource.Date != null)
+                        addField ("Fecha de publicación", docSource.Date.Value.ToShortDateString ());
+                    if (docSource.AccessDate != null)
+                        addField ("Fecha de acceso", docSource.AccessDate.Value.ToShortDateString ());
+                    if (docSource.AdditionalInfo != null)
+                        addField ("Información adicional", docSource.AdditionalInfo);
+                    if (docSource.ReliabilityLevel != null)
+                        addField ("Nivel de confiabilidod", docSource.ReliabilityLevel.Name);
+                    addField ("Observaciones", docSource.Observations);
+                    addNewline();
                 }
-                addNewline ();
             }
+
+            if (acase.InformationSources.Count > 0)
+            {
+                addTitle ("Fuentes de información personal");
+
+                foreach (InformationSource infoSource in acase.InformationSources) {
+                    if (infoSource.SourcePerson != null)
+                        addField ("Nombre", infoSource.SourcePerson.Fullname);
+                    if (infoSource.Date != null)
+                        addField ("Fecha", infoSource.Date.Value.ToShortDateString ());
+                    if (infoSource.SourceAffiliationType != null)
+                        addField ("Tipo de afiliación", infoSource.SourceAffiliationType.Name);
+                    addField ("Observaciones", infoSource.Observations);
+                    addNewline ();
+                }
+            }
+
+            if (acase.CaseRelationships.Count > 0)
+            {
+                addTitle ("Relación entre casos");
+
+                foreach (CaseRelationship relation in acase.CaseRelationships) {
+                    if (relation.Case != null)
+                        addField("Caso", relation.Case.Name);
+                    if (relation.RelationshipType != null)
+                        addField ("Tipo de relación", relation.RelationshipType.Name);
+                    if (relation.RelatedCase != null) {
+                        addField("Caso relacionado", relation.RelatedCase.Name);
+                        if (relation.RelatedCase.start_date != null)
+                            addField("Fecha de inicio", relation.RelatedCase.start_date.Value.ToShortDateString ());
+                        if (relation.RelatedCase.end_date != null)
+                            addField("Fecha de término", relation.RelatedCase.end_date.Value.ToShortDateString ());
+                    }
+                    addNewline ();
+                }
+            }
+
         }
     }
 }
