ce314ad88e26b17f1f594f7e3b06baf8fa59a0e2
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2012-04-19 10:24:48 -0700

    Updating synxtax of models

--- tools/database/models/institution_relationship.rb
+++ tools/database/models/institution_relationship.rb
@@ -2,6 +2,6 @@ class InstitutionRelationship < ActiveRecord::Base
   belongs_to :institution
   belongs_to :related_institution
   belongs_to :institution_relationship_type
-  belongs_to :start_date_type, :class_name => DateType, :foreign_key => 'start_date_type_id'
-  belongs_to :end_date_type, :class_name => DateType, :foreign_key => 'end_date_type_id'
+  belongs_to :start_date_type, :class_name => 'DateType', :foreign_key => 'start_date_type_id'
+  belongs_to :end_date_type, :class_name => 'DateType', :foreign_key => 'end_date_type_id'
 end
--- tools/database/models/person_relationship.rb
+++ tools/database/models/person_relationship.rb
@@ -2,6 +2,6 @@ class PersonRelationship < ActiveRecord::Base
   belongs_to :person
   belongs_to :related_person
   belongs_to :person_relationship_type
-  belongs_to :start_date_type, :class_name => DateType, :foreign_key => 'start_date_type_id'
-  belongs_to :end_date_type, :class_name => DateType, :foreign_key => 'end_date_type_id'
+  belongs_to :start_date_type, :class_name => "DateType", :foreign_key => 'start_date_type_id'
+  belongs_to :end_date_type, :class_name => "DateType", :foreign_key => 'end_date_type_id'
 end
